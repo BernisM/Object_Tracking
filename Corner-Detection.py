9805bd25131bccec6c71d74e909056bb0c99c64c
unknown <hellobernis@outlook.com>
2022-12-18 09:41:32 +0100

    first commit

---
+++ Corner-Detection.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline 
+
+flat_chess = cv2.imread('flat_chessboard.png')
+flat_chess = cv2.cvtColor(flat_chess, cv2.COLOR_BGR2RGB)
+
+plt.imshow(flat_chess)
+
+gray_flat_chess = cv2.cvtColor(flat_chess, cv2.COLOR_BGR2GRAY)
+
+plt.imshow(gray_flat_chess,cmap='gray')
+
+real_chess = cv2.imread('real_chessboard.jpg')
+real_chess = cv2.cvtColor(real_chess, cv2.COLOR_BGR2RGB)
+plt.imshow(real_chess)
+
+gray_real_chess = cv2.cvtColor(real_chess, cv2.COLOR_BGR2GRAY)
+plt.imshow(gray_real_chess,cmap='gray')
+
+gray = np.float32(gray_flat_chess)
+
+gray
+
+dst = cv2.cornerHarris(src=gray, blockSize=2, ksize=3, k=0.04)
+dst = cv2.dilate(dst, None)
+flat_chess[dst>0.01]
