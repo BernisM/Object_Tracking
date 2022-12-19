eed2ec89cfe9f0d6e3184e9ff20f1166e037a4ee
unknown <hellobernis@outlook.com>
2022-12-19 16:03:48 +0100

    upd2

--- Feature-Matching.py
+++ Feature-Matching.py
@@ -72,3 +72,79 @@ for match1,match2 in matches:
 sift_matches = cv2.drawMatchesKnn(reeses,kp1,cereals,kp2,good,None,flags=2)
 
 display(sift_matches)
+
+
+# =============================================================================
+# FLANN based Matcher
+# =============================================================================
+# Create SIFT Object
+sift = cv2.xfeatures2d.SIFT_create()
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(reeses,None)
+kp2, des2 = sift.detectAndCompute(cereals,None)
+
+ # FLANN - FAST LIBRARY FOR APPROXIMATE NEAREST NEIGHBORS
+# find general good matches
+
+FLANN_INDEX_KDTREE = 0
+index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees=5)
+search_params = dict(checks=50)
+ 
+flann = cv2.FlannBasedMatcher(index_params,search_params)
+
+matches = flann.knnMatch(des1,des2,k=2)
+
+good = []
+
+# ratio test
+for i,(match1,match2) in enumerate(matches):
+    if match1.distance < 0.7*match2.distance:
+        
+        good.append([match1])
+
+
+flann_matches = cv2.drawMatchesKnn(reeses,kp1,cereals,kp2,good,None,flags=0)
+
+display(flann_matches)
+
+
+--------------------------------------------------------------
+
+# Initiate SIFT detector
+sift = cv2.xfeatures2d.SIFT_create()
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(reeses,None)
+kp2, des2 = sift.detectAndCompute(cereals,None)
+
+# FLANN parameters
+FLANN_INDEX_KDTREE = 0
+index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+search_params = dict(checks=50)  
+
+flann = cv2.FlannBasedMatcher(index_params,search_params)
+
+matches = flann.knnMatch(des1,des2,k=2)
+
+# Need to draw only good matches, so create a mask
+matchesMask = [[0,0] for i in range(len(matches))]
+
+# ratio test
+for i,(match1,match2) in enumerate(matches):
+    if match1.distance < 0.7*match2.distance:
+        matchesMask[i]=[1,0]
+
+draw_params = dict(matchColor = (0,255,0),
+                   singlePointColor = (255,0,0),
+                   matchesMask = matchesMask,
+                   flags = 0)
+
+flann_matches = cv2.drawMatchesKnn(reeses,kp1,cereals,kp2,matches,None,**draw_params)
+
+
+
+
+
+
+
